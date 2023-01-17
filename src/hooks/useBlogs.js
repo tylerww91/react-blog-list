7240395edb8dc3b75e21d060b252492330594919
Tyler Watson <tylerww91@gmail.com>
2023-01-17 12:29:50 -0800

    creates blogs state variable and setBlogs setter using useState and ties the setter within useEffect

--- src/hooks/useBlogs.js
+++ src/hooks/useBlogs.js
@@ -1,1 +1,15 @@
-// import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { getBlogs } from '../services/blogs.js';
+
+export function useBlogs() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await getBlogs();
+      setBlogs(resp);
+    };
+    fetchData();
+  }, []);
+  return blogs;
+}
